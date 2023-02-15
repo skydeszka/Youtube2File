dcab7ed5c72a06ca0161ba746febd3eaa9b16067
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2023-02-15 22:14:04 +0100

    removed: GetPreferredAudioOrHighestBitrate

--- YoutubeService.cs
+++ YoutubeService.cs
@@ -23,35 +23,14 @@ public sealed class YoutubeDownloader
         _client = new();
     }
 
-    /// <summary>
-    /// Gets the audio in the first found preferred <see cref="Container"/> type
-    /// or if no match found gets the one with the highest bitrate.
-    /// </summary>
-    /// <param name="containers">The prefered containers.</param>
-    /// <returns>The prefered or best quality <see cref="IStreamInfo"/>.</returns>
-    private async ValueTask<IStreamInfo> GetPreferredAudioOrHighestBitrate(params Container[] containers)
     {
-        var manifest = await GetManifest();
 
-        var audioOnlyStreams = manifest.GetAudioOnlyStreams();
 
-        IStreamInfo? stream = null;
 
-        foreach (Container container in containers)
-        {
-            var matches = audioOnlyStreams
-                .Where(stream => stream.Container == container);
 
-            if (!matches.Any())
-                continue;
 
-            stream = matches.GetWithHighestBitrate();
-        }
 
-        stream ??= audioOnlyStreams.GetWithHighestBitrate();
 
-        return stream;
-    }
 
         string extension = streamInfo.Container.ToString();
 
